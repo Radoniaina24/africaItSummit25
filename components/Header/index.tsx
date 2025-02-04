427172c0a26f3dba86cc6a7696591ce85df1eefb
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-02-04 13:52:22 +0300

    v1.0

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -40,7 +40,7 @@ const Header = () => {
 
   return (
     <header
-      className={`fixed left-0 top-0 z-99999 w-full  transition-transform duration-300 ${
+      className={`fixed left-0 top-0 z-99999 w-full bg-black !bg-opacity-95 backdrop-blur-sm transition-transform duration-300 ${
         showHeader ? "translate-y-0" : "-translate-y-full"
       } ${
         stickyMenu
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -26,30 +26,26 @@ const Hero = () => {
       {/* Content */}
       <div className="relative z-10 flex h-full items-center justify-center px-6 text-white">
         <div className="container">
-          <div className="flex flex-col items-center">
-            <div className="flex items-center">
+          <div className="flex flex-col items-center gap-4">
+            <div className="">
               <Image
                 src={
                   "https://res.cloudinary.com/dx3xhdaym/image/upload/v1738657222/Africa_IT_Summit_Logo-blac_texte-06_xvd7my.png"
                 }
                 alt={"logo"}
-                width={350}
-                height={350}
+                width={500}
+                height={500}
                 className="bg-transparent opacity-95"
               />
-
-              <p className=" -ml-17 bg-transparent text-4xl font-extrabold text-gray-300 drop-shadow-md">
+            </div>
+            <div>
+              <p className=" -ml-5 bg-transparent text-4xl font-extrabold text-gray-300 drop-shadow-md">
                 {tech.toUpperCase()}
               </p>
             </div>
-            {/* <div>
-              <Image
-                src={"/images/logo/logo.png"}
-                alt={"logo"}
-                width={250}
-                height={250}
-              />
-            </div> */}
+            <div className="">
+              <ProfessionalDesign />
+            </div>
             {/* <div>
               <Image
                 src={"/images/logo/logo.png"}
@@ -66,3 +62,39 @@ const Hero = () => {
 };
 
 export default Hero;
+const ProfessionalDesign = () => {
+  const items = [
+    {
+      text: "Innovation",
+      gradient: "from-blue-500 via-purple-500 to-pink-500",
+    },
+    {
+      text: "Opportunities",
+      gradient: "from-green-500 via-teal-500 to-blue-500",
+    },
+    {
+      text: "Leadership",
+      gradient: "from-yellow-500 via-orange-500 to-red-500",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col items-center justify-center p-6">
+      <div className="flex flex-wrap justify-center gap-10">
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className={`text-md relative flex h-32 w-32 items-center justify-center rounded-full bg-transparent p-4 font-bold text-white shadow-lg md:h-40 md:w-40 md:text-xl`}
+          >
+            <span className="absolute left-1/2 top-1/2 z-99999 -translate-x-1/2 -translate-y-1/2 transform text-center">
+              {item.text}
+            </span>
+            <div
+              className={`absolute inset-0 rounded-full border-4 border-transparent bg-gradient-to-r ${item.gradient} animate-border`}
+            ></div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
