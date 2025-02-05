4d05ecd403381a95c2dd89d82c3ecdf4fa256f1f
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-02-05 15:06:28 +0300

    1.7

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -34,8 +34,8 @@ export default function Home() {
       <CTA />
       <FAQ />
       <Testimonial />
-      <Pricing />
-      <Contact /> */}
+      <Pricing /> */}
+      <Contact />
       {/* <Blog /> */}
     </main>
   );
