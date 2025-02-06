150cb0602270cb80c62cc9aaba34afe50f520689
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-02-06 10:29:04 +0300

    v2.3

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -33,8 +33,8 @@ export default function Home() {
       <Integration />
       <CTA />
       <FAQ />
-      <Testimonial />
-      <Pricing /> */}
+      <Testimonial /> */}
+      <Pricing />
       <Contact />
       {/* <Blog /> */}
     </main>
